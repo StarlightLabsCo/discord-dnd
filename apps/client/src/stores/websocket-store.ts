975b78793bb9422a0237da099be4c9ff72335859
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-03-24 10:29:59 -0700

    Add exponential backoff + retry logic for client ws.

--- apps/client/src/stores/websocket-store.ts
+++ apps/client/src/stores/websocket-store.ts
@@ -1,34 +1,66 @@
 import { create } from "zustand";
 
-type WebsocketStore = {
+type WebSocketStore = {
     ws: WebSocket | null;
     connect: () => void;
+    exponentialBackoff: number;
 };
 
-export const useWebsocketStore = create<WebsocketStore>((set, get) => ({
+type WebSocketStoreSet = (arg0: {
+    ws?: WebSocket | null;
+    connect?: () => void;
+    exponentialBackoff?: number;
+}) => void;
+
+export const useWebsocketStore = create<WebSocketStore>((set, get) => ({
     ws: null,
-
-    connect: () => {
-        try {
-            console.log("[WS] Connecting");
-
-            const ws = new WebSocket(`wss://${location.host}/api/ws`);
-
-            ws.onopen = () => {
-                console.log("[WS] Connected");
-                set({ ws });
-            };
-
-            ws.onmessage = (event) => {
-                console.log(`[WS] Message: ${event.data}`);
-            };
-
-            ws.onclose = () => {
-                console.log("[WS] Disconnected");
-                set({ ws: null });
-            };
-        } catch (error) {
-            console.error("[WS] Error:", error);
-        }
-    },
+    connect: () => connect(set, get),
+    exponentialBackoff: 1000,
 }));
+
+async function connect(set: WebSocketStoreSet, get: () => WebSocketStore) {
+    try {
+        console.log("[WS] Connecting");
+
+        const ws = new WebSocket(`wss://${location.host}/api/ws`);
+
+        ws.onopen = () => {
+            console.log("[WS] Connected");
+            set({ ws });
+        };
+
+        ws.onmessage = (event) => {
+            console.log(`[WS] Message: ${event.data}`);
+        };
+
+        ws.onerror = (error) => {
+            console.error("[WS] Error:", error);
+        };
+
+        ws.onclose = (event: CloseEvent) => {
+            set({
+                ws: null,
+            });
+
+            console.log(
+                `[WS] WebSocket connection closed. Code: ${event.code} Reason: ${event.reason}`
+            );
+
+            if (!event.wasClean) {
+                // TODO: log error to sentry
+            }
+
+            retry(set, get);
+        };
+    } catch (error) {
+        console.error("[WS] Error:", error);
+        retry(set, get);
+    }
+}
+
+async function retry(set: WebSocketStoreSet, get: () => WebSocketStore) {
+    setTimeout(() => {
+        set({ exponentialBackoff: get().exponentialBackoff * 2 });
+        get().connect();
+    }, get().exponentialBackoff);
+}

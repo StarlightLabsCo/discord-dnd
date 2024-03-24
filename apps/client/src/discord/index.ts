27e426543f205e2738d56c044057d7cfe0eb828b
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-03-23 20:12:54 -0700

    Add setup comment.

--- apps/client/src/discord/index.ts
+++ apps/client/src/discord/index.ts
@@ -4,6 +4,8 @@ import { authenticate } from "./auth";
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
 async function setup() {
+    console.log("Setting up Discord SDK");
+
     await discordSdk.ready();
     console.log("Discord SDK ready");
 

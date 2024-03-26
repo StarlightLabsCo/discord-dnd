395004f37f262e913063b112ab04b1245dd8b4bf
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-03-25 23:18:06 -0700

    Proper vite resolve.

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import * as path from "path";
 
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
 });

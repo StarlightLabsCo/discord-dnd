7b54b8a2db3c73084ddb6f4989e4faf3c916326c
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-04-23 13:25:25 -0700

    Move instance state to it's own file.

--- apps/server/src/api/websocket/connection.ts
+++ apps/server/src/api/websocket/connection.ts
@@ -1,16 +1,11 @@
 import type { ServerWebSocket } from "bun";
-import { server } from "index";
 import type { WebSocketData } from ".";
 
-import type {
-    InstanceState,
-    UserInfoResponse,
-    InstanceStateResponse,
-} from "starlight-api-types/websocket";
-import { db } from "@/lib/db";
-import type { User } from "database";
-
-const idToState = new Map<string, InstanceState>();
+import type { UserInfoResponse } from "starlight-api-types/websocket";
+import {
+    removeUserFromInstanceState,
+    addUserToInstanceState,
+} from "./instanceState";
 
 export async function handlePlayerConnect(ws: ServerWebSocket<WebSocketData>) {
     const { instanceId, user } = ws.data;
@@ -24,81 +19,7 @@ export async function handlePlayerConnect(ws: ServerWebSocket<WebSocketData>) {
     ws.send(JSON.stringify(userInfoResponse));
 
     // Create or update the instance state
-    let instanceState = idToState.get(instanceId);
-    if (!instanceState) {
-        let selectedCampaign = await findOrCreateCampaignForUser(user);
-
-        instanceState = {
-            state: "LOBBY",
-            connectedPlayers: [
-                {
-                    user,
-                    character: null,
-                    status: "NOT_READY",
-                },
-            ],
-            selectedCampaign,
-        };
-        idToState.set(instanceId, instanceState);
-    } else {
-        instanceState.connectedPlayers.push({
-            user,
-            character: null,
-            status: "NOT_READY",
-        });
-    }
-
-    const instanceStateResponse: InstanceStateResponse = {
-        type: "InstanceStateResponse",
-        data: instanceState,
-    };
-    server.publish(instanceId, JSON.stringify(instanceStateResponse));
-}
-
-async function findOrCreateCampaignForUser(user: User) {
-    let campaignInstance = await db.campaignInstance.findFirst({
-        where: {
-            users: {
-                some: {
-                    id: user.id,
-                },
-            },
-        },
-        orderBy: {
-            updatedAt: "desc",
-        },
-    });
-
-    if (!campaignInstance) {
-        const campaign = await db.campaign.findFirst({
-            orderBy: {
-                updatedAt: "desc",
-            },
-        });
-        if (!campaign) {
-            throw new Error("No campaigns found");
-        }
-
-        campaignInstance = await db.campaignInstance.create({
-            data: {
-                campaign: {
-                    connect: {
-                        id: campaign.id,
-                    },
-                },
-                users: {
-                    connect: {
-                        id: user.id,
-                    },
-                },
-                name: campaign.name,
-                description: campaign.description,
-                imageUrl: campaign.imageUrl,
-            },
-        });
-    }
-
-    return campaignInstance;
+    await addUserToInstanceState(instanceId, user);
 }
 
 export async function handlePlayerDisconnect(
@@ -107,21 +28,5 @@ export async function handlePlayerDisconnect(
     const { instanceId, user } = ws.data;
     ws.unsubscribe(instanceId);
 
-    const instanceState = idToState.get(instanceId);
-    if (!instanceState) return;
-
-    instanceState.connectedPlayers = instanceState.connectedPlayers.filter(
-        (player) => player.user.id !== user.id
-    );
-
-    if (instanceState.connectedPlayers.length === 0) {
-        idToState.delete(instanceId);
-    } else {
-        idToState.set(instanceId, instanceState);
-        const instanceStateResponse: InstanceStateResponse = {
-            type: "InstanceStateResponse",
-            data: instanceState,
-        };
-        server.publish(instanceId, JSON.stringify(instanceStateResponse));
-    }
+    await removeUserFromInstanceState(instanceId, user);
 }
---
+++ apps/server/src/api/websocket/instanceState.ts
@@ -0,0 +1,110 @@
+import { db } from "@/lib/db";
+import type { User } from "database";
+import type {
+    InstanceState,
+    InstanceStateResponse,
+} from "starlight-api-types/websocket";
+import { server } from "index";
+
+const idToState = new Map<string, InstanceState>();
+
+export async function addUserToInstanceState(instanceId: string, user: User) {
+    let instanceState = idToState.get(instanceId);
+    if (!instanceState) {
+        let selectedCampaign = await findOrCreateCampaignForUser(user);
+
+        instanceState = {
+            state: "LOBBY",
+            connectedPlayers: [
+                {
+                    user,
+                    character: null,
+                    status: "NOT_READY",
+                },
+            ],
+            selectedCampaign,
+        };
+        idToState.set(instanceId, instanceState);
+    } else {
+        instanceState.connectedPlayers.push({
+            user,
+            character: null,
+            status: "NOT_READY",
+        });
+    }
+
+    const instanceStateResponse: InstanceStateResponse = {
+        type: "InstanceStateResponse",
+        data: instanceState,
+    };
+    server.publish(instanceId, JSON.stringify(instanceStateResponse));
+}
+
+async function findOrCreateCampaignForUser(user: User) {
+    let campaignInstance = await db.campaignInstance.findFirst({
+        where: {
+            users: {
+                some: {
+                    id: user.id,
+                },
+            },
+        },
+        orderBy: {
+            updatedAt: "desc",
+        },
+    });
+
+    if (!campaignInstance) {
+        const campaign = await db.campaign.findFirst({
+            orderBy: {
+                updatedAt: "desc",
+            },
+        });
+        if (!campaign) {
+            throw new Error("No campaigns found");
+        }
+
+        campaignInstance = await db.campaignInstance.create({
+            data: {
+                campaign: {
+                    connect: {
+                        id: campaign.id,
+                    },
+                },
+                users: {
+                    connect: {
+                        id: user.id,
+                    },
+                },
+                name: campaign.name,
+                description: campaign.description,
+                imageUrl: campaign.imageUrl,
+            },
+        });
+    }
+
+    return campaignInstance;
+}
+
+export async function removeUserFromInstanceState(
+    instanceId: string,
+    user: User
+) {
+    const instanceState = idToState.get(instanceId);
+    if (!instanceState) return;
+
+    instanceState.connectedPlayers = instanceState.connectedPlayers.filter(
+        (player) => player.user.id !== user.id
+    );
+
+    if (instanceState.connectedPlayers.length === 0) {
+        idToState.delete(instanceId);
+    } else {
+        idToState.set(instanceId, instanceState);
+        const instanceStateResponse: InstanceStateResponse = {
+            type: "InstanceStateResponse",
+            data: instanceState,
+        };
+        server.publish(instanceId, JSON.stringify(instanceStateResponse));
+    }
+}

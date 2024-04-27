326bd76aab6c60271db7b0a981dd3a93fdb00248
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-04-27 14:29:36 -0700

    Add handler for adding messages.

--- apps/client/src/lib/websocket/handlers/messageAdded.ts
+++ apps/client/src/lib/websocket/handlers/messageAdded.ts
@@ -1,5 +1,21 @@
+import { useGameStore } from "@/lib/game";
 import { MessageAddedResponse } from "starlight-api-types/websocket";
 
 export async function handleMessageAddedResponse(
     response: MessageAddedResponse
-) {}
+) {
+    const { state, setState } = useGameStore.getState();
+    if (!state) return;
+
+    if (state.selectedCampaign) {
+        const message = response.data;
+        const updatedMessages = [...state.selectedCampaign.messages, message];
+        setState({
+            ...state,
+            selectedCampaign: {
+                ...state.selectedCampaign,
+                messages: updatedMessages,
+            },
+        });
+    }
+}

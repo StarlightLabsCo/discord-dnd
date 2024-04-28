32d31e65b09e2dbb5806b74d39718f4155796085
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-04-27 21:27:04 -0700

    Swap imageUrl to work on discord.

--- apps/client/src/components/game/MessageDisplay.tsx
+++ apps/client/src/components/game/MessageDisplay.tsx
@@ -17,11 +17,7 @@ export function MessageDisplay({ message, className }: Props) {
         <div className={cn("flex gap-x-[1.5vw]", className)}>
             <div className='shrink-0'>
                 <img
-                    src={
-                        character
-                            ? character.imageUrl
-                            : "https://cdn.midjourney.com/9fc0b2ca-d4dd-498e-8e4f-0540407f4c1c/0_2.webp"
-                    }
+                    src={character ? character.imageUrl : "/r2/dm.webp"}
                     className='rounded-full w-[3vw] h-[3vw] object-cover'
                 />
             </div>

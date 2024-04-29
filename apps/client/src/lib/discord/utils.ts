35dc7cbb1ca13d8cc35dde01b9e5cb807e201f23
Harris Rothaermel <harris.rothaermel@gmail.com>
2024-04-29 14:35:32 -0700

    Fix URL rewriter

--- apps/client/src/lib/discord/utils.ts
+++ apps/client/src/lib/discord/utils.ts
@@ -1,6 +1,6 @@
 export function s3UrlRewriter(url: string) {
     if (process.env.NODE_ENV === "production") {
-        return url.replace("https://r2.starlightlabs.co/", "/r2/");
+        return url.replace("r2.starlightlabs.co/", "/r2/");
     }
     return url;
 }
